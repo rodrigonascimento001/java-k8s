4d23090af70aabe6555c2cc9c4b54882422823f0
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2025-09-20 20:56:10 -0300

    first commit

---
+++ src/main/java/com/br/rmm/javak8s/Javak8sApplication.java
@@ -0,0 +1,13 @@
+package com.br.rmm.javak8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javak8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javak8sApplication.class, args);
+	}
+
+}
