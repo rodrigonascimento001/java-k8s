f040e902a3db75c4463241de3403983a0529e91a
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2025-09-26 17:08:31 -0300

    update ci-cd.yaml

--- src/main/java/com/br/rmm/javak8s/controllers/KubeController.java
+++ src/main/java/com/br/rmm/javak8s/controllers/KubeController.java
@@ -1,5 +1,7 @@
 package com.br.rmm.javak8s.controllers;
 
+import com.br.rmm.javak8s.models.AppResponse;
+import com.br.rmm.javak8s.service.KubeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/kube")
 public class KubeController {
+    private final KubeService kubeService;
+
+    public KubeController(KubeService kubeService) {
+        this.kubeService = kubeService;
+    }
+
     @GetMapping
-    public ResponseEntity<String> helloKube(){
-        return ResponseEntity.ok("Hello Java Kube =)");
+    public ResponseEntity<AppResponse> helloKube(){
+        return ResponseEntity.ok(kubeService.validateApp());
     }
 }
