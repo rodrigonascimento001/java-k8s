f7d9f930cb04f57038d08a62f1e203170a6b69ad
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2025-10-14 22:52:48 -0300

    update ci-cd.yaml

--- src/main/java/com/br/rmm/javak8s/service/KubeServiceImpl.java
+++ src/main/java/com/br/rmm/javak8s/service/KubeServiceImpl.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public final class KubeServiceImpl implements KubeService {
     @Override
     public AppResponse validateApp() {
-        return new AppResponse("::Hello Java Kube =) v2");
+        return new AppResponse("::Hello Java Kube =) v3");
     }
 }
