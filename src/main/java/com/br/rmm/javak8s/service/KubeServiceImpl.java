98bc32eb9171eb0b8b7330157559ab000f95be12
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2025-10-14 23:52:14 -0300

    update ci-cd.yaml

--- src/main/java/com/br/rmm/javak8s/service/KubeServiceImpl.java
+++ src/main/java/com/br/rmm/javak8s/service/KubeServiceImpl.java
@@ -1,12 +1,18 @@
 package com.br.rmm.javak8s.service;
+import org.slf4j.Logger;
 
 import com.br.rmm.javak8s.models.AppResponse;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class KubeServiceImpl implements KubeService {
+    Logger logger  = LoggerFactory.getLogger(KubeServiceImpl.class);
+
     @Override
     public AppResponse validateApp() {
-        return new AppResponse("::Hello Java Kube =) v4");
+        var response = new AppResponse("::Hello Java Kube =) v4");
+        logger.info("Response: " + response);
+        return response;
     }
 }
